74a6a9d2eac0d29406c72341c578763526831ee2
Rebecca <rebecca.solomon446@gmail.com>
2023-02-16 15:17:15 +0000

    makes changes

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,6 +2,7 @@ import Comment from "./Comment";
 import { useState, useContext } from "react";
 import CommentForm from "./CommentForm";
 import { UserContext } from "../App";
+import { Link } from "react-router-dom";
 
 
 const SERVER_URL = "http://localhost:8080"
