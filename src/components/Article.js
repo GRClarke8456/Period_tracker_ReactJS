bc8fdafdd708553cc478819ac863aa19d8ec6f22
Rebecca <rebecca.solomon446@gmail.com>
2023-02-16 17:56:39 +0000

    makes changes

--- src/components/Article.js
+++ src/components/Article.js
@@ -17,6 +17,10 @@ const Article = ({article}) => {
     const [comments, setComments] = useState(article.comments)
     const [numOfLikes, setNumOfLikes] = useState(article.numOfLikes);
 
+    const[showCommentForm, setShowCommentForm] = useState(false);
+
+    const [expanded, setExpanded] = useState(false);
+
     const commentComponent = comments.map((comment) => {
         return <Comment key={comment.id} comment={comment} />
     })
@@ -57,53 +61,62 @@ const Article = ({article}) => {
           }
       };
 
+      const handleCommentClick = () => {
+        setShowCommentForm(!showCommentForm);
+      };
+
 
     return ( 
         <>
-
-
-        <section>    
-            <div className="blog-card spring-fever">
-                <div className="title-content">
-                    {/* <h3><a href="#"> <{article.title}></a></h3> */}
-
-                    <h3>{article.title}</h3>
-                    <div className="intro"> <a href="#">Women and Lifestyle</a> </div>
-                </div>
-                <div className="card-info">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim... 
-                    {/* <Link  className="licon icon-arr icon-black" to="/article/articleinfo">Read Article</Link> */}
-                    <Link to="/article/articleinfo">Read Article</Link>
-                    {/* <Link className="linkbutton" to="/">Log Out</Link> */}
-                    {/* <Link className="licon icon-arr icon-black" to={`/articles/${article.id}`}> Read Article </Link> */}
-                    {/* <a href="#">Read Article<span className="licon icon-arr icon-black"></span></a> */}
-
-                </div>
-                <div className="utility-info">
-                <ul className="utility-list">
-                {/* <li><span className="licon icon-like"></span><a href="#">2</a></li> */}
-                <li onClick={handleLikeClick}><span className="licon icon-like"></span> {numOfLikes} </li>
-                
-                <li><span className="licon icon-com"></span> ... </li>
-                <li><span className="licon icon-dat"></span> {article.date} </li>
-                <li><span className="licon icon-tag"></span> {article.tag} </li>
-                </ul>
-                </div>
-                <div className="gradient-overlay"></div>
-                <div className="color-overlay"></div>
+         <section>
+        <div
+          className={`blog-card spring-fever${expanded ? " expanded" : ""}`}
+          onClick={() => setExpanded(!expanded)}
+        >
+          <div className="title-content">
+            <h3>{article.title}</h3>
+            <div className="intro">
+              <a href="#">Women and Lifestyle</a>
             </div>
-                <p>Reviews:</p>
-                <ul>
-                    {commentComponent}
-                </ul>
-                <CommentForm postNewComment={postNewComment} articleId={article.id} />
-                {/* <p>{article.articleslikes}</p>
-                <p>{article.numOfLikes}</p>
-                <p>{article.comments}</p> */}
-
-            </section>
-
-
+          </div>
+          <div className="card-info">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim...
+            <Link to="/article/articleinfo">Read Article</Link>
+          </div>
+          <div className="utility-info">
+            <ul className="utility-list">
+              <li onClick={handleLikeClick}>
+                <span className="licon icon-like"></span> {numOfLikes}{" "}
+              </li>
+              <li onClick={handleCommentClick}>
+                <span className="licon icon-com"></span> ...
+              </li>
+              <li>
+                <span className="licon icon-dat"></span> {article.date}
+              </li>
+              <li>
+                <span className="licon icon-tag"></span> {article.tag}
+              </li>
+            </ul>
+          </div>
+          <div className="gradient-overlay"></div>
+          <div className="color-overlay"></div>
+          {expanded && (
+            <>
+              <div className="comment-section">
+                <ul>{commentComponent}</ul>
+                <CommentForm
+                  postNewComment={postNewComment}
+                  articleId={article.id}
+                />
+              </div>
+              <div className="close-button" onClick={() => setExpanded(false)}>
+                Close
+              </div>
+            </>
+          )}
+        </div>
+    </section>
         </>
 
      );
